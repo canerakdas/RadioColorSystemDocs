6975ed4d2501be7b36303188a9ff5015f535a0f4
Caner Akdas <canerakdas@gmail.com>
2023-04-07 00:02:01 +0300

    chore: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,95 @@
+<script>
+	import Header from './Header.svelte';
+	import { RadioStatic } from 'radio-color-system';
+
+	const colors = [
+		{ color: { h: 200, s: 12, l: 60 }, name: 'secondary' },
+		{
+			color: { h: 240, s: 60, l: 70 },
+			name: 'primary'
+		},
+		{ color: { h: 342, s: 25, l: 61 }, name: 'tertiary' },
+		{ color: { h: 270, s: 2, l: 57 }, name: 'neutral' }
+	];
+</script>
+
+<RadioStatic {colors}>
+	<Header />
+	<div class="card secondary-80 secondary-font-80">
+		<h3>
+			<span class="secondary-80 secondary-font-80">Header</span>
+			<span class="underline">not header</span>
+		</h3>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis aliquet erat eget
+			facilisis. Phasellus fermentum semper ante dictum vehicula. Class aptent taciti sociosqu ad
+			litora torquent per conubia nostra, per inceptos himenaeos. Etiam sit amet ligula risus. Donec
+			et pulvinar nibh. Etiam augue odio, auctor eget ornare sit amet, sodales eu orci. Praesent
+			hendrerit feugiat libero vel vulputate. Maecenas id molestie felis. Praesent convallis sapien
+			est, at gravida metus viverra in. Nunc blandit, metus a rhoncus mattis, nibh ex tristique
+			odio, eget accumsan risus ligula sed sem. Mauris hendrerit ultricies accumsan. Donec dictum
+			diam ac libero facilisis dictum sed et sem.
+		</p>
+	</div>
+	<div class="card neutral-80">asdfs</div>
+
+	<div class="card secondary-80 secondary-font-80">
+		<h3>
+			<span class="secondary-80 secondary-font-80">Header</span>
+			<span class="underline">not header</span>
+		</h3>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis aliquet erat eget
+			facilisis. Phasellus fermentum semper ante dictum vehicula. Class aptent taciti sociosqu ad
+			litora torquent per conubia nostra, per inceptos himenaeos. Etiam sit amet ligula risus. Donec
+			et pulvinar nibh. Etiam augue odio, auctor eget ornare sit amet, sodales eu orci. Praesent
+			hendrerit feugiat libero vel vulputate. Maecenas id molestie felis. Praesent convallis sapien
+			est, at gravida metus viverra in. Nunc blandit, metus a rhoncus mattis, nibh ex tristique
+			odio, eget accumsan risus ligula sed sem. Mauris hendrerit ultricies accumsan. Donec dictum
+			diam ac libero facilisis dictum sed et sem.
+		</p>
+	</div>
+	<div class="card primary-80 primary-font-80">
+		<h3>
+			<span class="primary-80 primary-font-80">Header</span>
+			<span class="underline">not header</span>
+		</h3>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis aliquet erat eget
+			facilisis. Phasellus fermentum semper ante dictum vehicula. Class aptent taciti sociosqu ad
+			litora torquent per conubia nostra, per inceptos himenaeos. Etiam sit amet ligula risus. Donec
+			et pulvinar nibh. Etiam augue odio, auctor eget ornare sit amet, sodales eu orci. Praesent
+			hendrerit feugiat libero vel vulputate. Maecenas id molestie felis. Praesent convallis sapien
+			est, at gravida metus viverra in. Nunc blandit, metus a rhoncus mattis, nibh ex tristique
+			odio, eget accumsan risus ligula sed sem. Mauris hendrerit ultricies accumsan. Donec dictum
+			diam ac libero facilisis dictum sed et sem.
+		</p>
+	</div>
+</RadioStatic>
+
+<style lang="scss">
+	.card {
+		width: 1024px;
+		margin: auto;
+		border-radius: 0.5rem;
+		padding: 1rem;
+		margin: 2rem auto;
+		p {
+			color: currentColor;
+		}
+	}
+
+	h3 {
+		margin: 0;
+		padding: 0;
+		display: flex;
+		flex-direction: column;
+		font-size: 2rem;
+		font-weight: 300;
+	}
+
+	.underline {
+		text-decoration: line-through;
+		font-size: 1rem;
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,197 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const t = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+	});
+</script>
+
+<div class="conta">
+	<header>
+		<div>
+			<h1 class="header-logo">
+				<span class="header-logo--main neutral-font-90">Radio</span>
+				<span class="header-logo--sub neutral-font-90">Color System</span>
+			</h1>
+			<div class="colors">
+				{#each [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100] as codes}
+					<div class={`primary-${codes} primary-font-${codes} color--item`} />
+				{/each}
+			</div>
+		</div>
+		<div class="vertical-container">
+			<div class="vertical">
+				<h2 class="neutral-font-70">Saturation</h2>
+				<input class="sat" type="range" list="tickmark" min="0" max="100" value="100" />
+
+				<datalist id="tickmark">
+					{#each [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100] as frequency}
+						<option
+							class={frequency % 10 === 0 ? 'neutral-font-90' : 'neutral-font-50'}
+							value={frequency}
+							label={frequency % 10 === 0 ? String(frequency) : '|'}
+						/>
+					{/each}
+				</datalist>
+			</div>
+			<div class="vertical">
+				<h2 class="neutral-font-70">Lightning</h2>
+				<input class="sat" type="range" list="tickmark" min="0" max="100" value="50" />
+
+				<datalist id="tickmark">
+					{#each [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100] as frequency}
+						<option
+							class={frequency % 10 === 0 ? 'neutral-font-90' : 'neutral-font-50'}
+							value={frequency}
+							label={frequency % 10 === 0 ? String(frequency) : '|'}
+						/>
+					{/each}
+				</datalist>
+			</div>
+		</div>
+		<div>
+			<div class="casette neutral-50">primary</div>
+		</div>
+		<div class="knob">
+			<div class="knob-container neutral-70">
+				<div class="primary-50 knob-button">
+					<div class="knob-button--inner neutral-70 neutral-font-70">360°</div>
+				</div>
+			</div>
+		</div>
+	</header>
+</div>
+
+<style lang="scss">
+	.conta {
+		width: 1024px;
+		margin: 0 auto;
+	}
+	.speaker {
+		margin: 2rem 0 0 0;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		border-radius: 2px;
+		background-color: var(--primary-80);
+		height: 3rem;
+	}
+	.speaker > svg > path {
+		fill: var(--primary-50);
+	}
+
+	.casette {
+		width: 200px;
+		height: 66px;
+	}
+
+	.knob-container {
+		width: 7rem;
+		height: 7rem;
+		border-radius: 50%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.knob-button {
+		width: 6rem;
+		height: 6rem;
+		border-radius: 50%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		&--inner {
+			width: calc(6rem - 2px);
+			height: calc(6rem - 2px);
+
+			border-radius: 50%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+	}
+
+	input[type='range'] {
+		-webkit-appearance: none;
+		height: 0;
+		background: var(--neutral-70);
+		border-radius: 2px;
+		border: 5px dashed var(--neutral-50);
+
+		@media (prefers-dark-interface) {
+			background: var(--neutral-dark-50);
+		}
+	}
+
+	input[type='range']::-webkit-slider-thumb {
+		position: relative;
+		top: -0.75rem;
+		-webkit-appearance: none;
+		height: 20px;
+		width: 40px;
+		border-radius: 1rem;
+		background: var(--primary-30);
+		cursor: ew-resize;
+		box-shadow: inset 0 0 0 2px var(--primary-30), inset 0 0 0 3px var(--neutral-70);
+		transition: background 0.3s ease-in-out;
+	}
+
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.header-logo {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		padding: 0.5rem 1rem;
+		border-radius: 4px;
+
+		&--main {
+			letter-spacing: 0.25;
+			font-size: 2.3rem;
+			font-weight: 400;
+		}
+
+		&--sub {
+			font-size: 1rem;
+			font-weight: 600;
+		}
+	}
+
+	h2 {
+		font-weight: 400;
+		margin: 0;
+		font-size: 1rem;
+	}
+
+	.vertical {
+		width: 500px;
+	}
+
+	datalist {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+		font-size: 0.75rem;
+		font-weight: 200;
+		padding-top: 0.25rem;
+	}
+
+	.tickmark--text {
+	}
+
+	.tickmark--tick {
+	}
+	input {
+		width: 100%;
+	}
+
+	.color--item {
+		width: 20px;
+	}
+</style>
