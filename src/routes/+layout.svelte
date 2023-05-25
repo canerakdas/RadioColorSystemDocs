e0c79b4d76de869c98ab5f8e3c6ca5a6428f71ef
Caner Akdas <canerakdas@gmail.com>
2023-05-25 20:58:54 +0300

    refactor: color harmony implemented

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,11 +12,9 @@
 	let colors = writable([
 		{
 			color: { h: 5, s: 32, l: 48 },
-			name: 'primary',
+			name: ['primary', 'secondary', 'tertiary'],
 			selected: true
 		},
-		{ color: { h: 240, s: 15, l: 54 }, name: 'secondary' },
-		{ color: { h: 30, s: 24, l: 61 }, name: 'tertiary' },
 		{ color: { h: 270, s: 2, l: 57 }, name: 'neutral' }
 	]);
 
