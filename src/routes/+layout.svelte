4425a6ea31680ddfd1c4d80caff051ccdb5453a4
Caner Akdas <canerakdas@gmail.com>
2023-05-25 21:08:14 +0300

    feat: vercel analytics added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,6 +19,11 @@
 	]);
 
 	setContext('colors', colors);
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <RadioStatic colors={$colors} async={true} />
